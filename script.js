24bf5eed829669302c29578fa1e989bad4b8fcd4
Devansh Rajput <rajputdevansh012@gmail.com>
2023-11-13 21:07:13 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let textbox = document.getElementById('textbox');
+
+textbox.addEventListener('input', function () {
+    var text = this.value;
+    let char = text.length;
+    document.getElementById('char').innerHTML = char;
+
+    text = text.trim();          //remove extra spaces from start & end
+    let words = text.split(" ");
+    let cleanList = words.filter(function (element) {
+        return element != "";   //remove all extra spaces & return clean list
+    })
+    document.getElementById('word').innerHTML = cleanList.length;
+})
